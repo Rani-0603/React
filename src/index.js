9c1f96dc703e42e72358c8eb4a1215ca57c87231
Rani-0603 <68508330+Rani-0603@users.noreply.github.com>
2020-07-20 00:36:42 +0530

    My first Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar';
+// import Grid from './components/Grid';
+
+ReactDOM.render(<Navbar />, document.querySelector('#root'));
+// ReactDOM.render(<Grid />, document.querySelector('#root'));
